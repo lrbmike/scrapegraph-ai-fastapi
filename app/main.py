8c6019268da9b4991b327a22f536d9c09d9f42d4
lrb <lrbmike@163.com>
2024-08-09 12:44:52 +0800

    add ping router with cron

--- app/main.py
+++ app/main.py
@@ -6,10 +6,17 @@ import sys
 import os
 
 from dotenv import load_dotenv, find_dotenv
+
 load_dotenv(find_dotenv())
 
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 
 app = FastAPI()
+
+
+@app.get("/ping")
+def ping():
+    return "pong"
+
 
 app.include_router(crawl.router)
